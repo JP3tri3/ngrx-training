28a39eb474d1ed018fd56906d6bc455b63796dce
JP3tri3 <JP3tri3@gmail.com>
2022-06-25 17:26:48 -0400

    added effects

--- libs/books-page/src/lib/book-api.effects.ts
+++ libs/books-page/src/lib/book-api.effects.ts
@@ -7,4 +7,50 @@ import { BooksPageActions, BooksApiActions } from '@book-co/books-page/actions';
 @Injectable()
 export class BooksApiEffects {
   constructor(private booksService: BooksService, private actions$: Actions) {}
+
+  loadBooks$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(BooksPageActions.enter),
+      exhaustMap(() => {
+        return this.booksService
+          .all()
+          .pipe(map((books) => BooksApiActions.booksLoaded({ books })));
+      })
+    );
+  });
+
+  createBook$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(BooksPageActions.createBook),
+      concatMap((action) =>
+        this.booksService
+          .create(action.book)
+          .pipe(map((book) => BooksApiActions.bookCreated({ book })))
+      )
+    )
+  );
+
+  updateBook$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(BooksPageActions.updateBook),
+      concatMap((action) =>
+        this.booksService
+          .update(action.bookId, action.changes)
+          .pipe(map((book) => BooksApiActions.bookUpdated({ book })))
+      )
+    )
+  );
+
+  deleteBook$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(BooksPageActions.deleteBook),
+      mergeMap((action) =>
+        this.booksService
+          .delete(action.bookId)
+          .pipe(
+            map(() => BooksApiActions.bookDeleted({ bookId: action.bookId }))
+          )
+      )
+    )
+  );
 }
--- libs/books-page/src/lib/books-page/books-page.component.ts
+++ libs/books-page/src/lib/books-page/books-page.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-// import { Observable } from 'rxjs';
-// import { BooksPageActions } from '@book-co/books-page/actions';
+import { Observable } from 'rxjs';
+import { BookModel, BookRequiredProps } from '@book-co/shared-models';
 import {
-  BookModel,
-  BookRequiredProps,
-  calculateBooksGrossEarnings,
-} from '@book-co/shared-models';
-import {} from '@book-co/shared-state-books';
-import { BooksService } from '@book-co/shared-services';
-import { BooksApiActions, BooksPageActions } from '@book-co/books-page/actions';
+  selectActiveBook,
+  selectAllBooks,
+  selectBooksEarningTotals,
+} from '@book-co/shared-state-books';
+import { BooksPageActions } from '@book-co/books-page/actions';
 
 @Component({
   selector: 'bco-books-page',
@@ -17,36 +15,24 @@ import { BooksApiActions, BooksPageActions } from '@book-co/books-page/actions';
   styleUrls: ['./books-page.component.scss'],
 })
 export class BooksPageComponent implements OnInit {
-  books: BookModel[] = [];
-  currentBook: BookModel | null = null;
-  // eslint-disable-next-line @typescript-eslint/no-inferrable-types
-  total: number = 0;
+  books$: Observable<BookModel[]>;
+  currentBook$: Observable<BookModel | null>;
+  total$: Observable<number>;
 
-  constructor(private booksService: BooksService, private store: Store) {}
+  constructor(private store: Store) {
+    this.books$ = store.select(selectAllBooks);
+    this.currentBook$ = store.select(selectActiveBook);
+    this.total$ = store.select(selectBooksEarningTotals);
+  }
 
   ngOnInit() {
-    this.getBooks();
     this.removeSelectedBook();
 
     this.store.dispatch(BooksPageActions.enter());
   }
 
-  getBooks() {
-    this.booksService.all().subscribe((books) => {
-      this.books = books;
-      this.updateTotals(books);
-      this.store.dispatch(BooksApiActions.booksLoaded({ books }));
-    });
-  }
-
-  updateTotals(books: BookModel[]) {
-    this.total = calculateBooksGrossEarnings(books);
-  }
-
   onSelect(book: BookModel) {
     this.store.dispatch(BooksPageActions.selectBook({ bookId: book.id }));
-
-    this.currentBook = book;
   }
 
   onCancel() {
@@ -55,8 +41,6 @@ export class BooksPageComponent implements OnInit {
 
   removeSelectedBook() {
     this.store.dispatch(BooksPageActions.clearSelectedBook());
-
-    this.currentBook = null;
   }
 
   onSave(book: BookRequiredProps | BookModel) {
@@ -69,36 +53,15 @@ export class BooksPageComponent implements OnInit {
 
   saveBook(bookProps: BookRequiredProps) {
     this.store.dispatch(BooksPageActions.createBook({ book: bookProps }));
-
-    this.booksService.create(bookProps).subscribe((book) => {
-      this.getBooks();
-      this.removeSelectedBook();
-
-      this.store.dispatch(BooksApiActions.bookCreated({ book }));
-    });
   }
 
   updateBook(book: BookModel) {
     this.store.dispatch(
       BooksPageActions.updateBook({ bookId: book.id, changes: book })
     );
-
-    this.booksService.update(book.id, book).subscribe((book) => {
-      this.getBooks();
-      this.removeSelectedBook();
-
-      this.store.dispatch(BooksApiActions.bookUpdated({ book }));
-    });
   }
 
   onDelete(book: BookModel) {
     this.store.dispatch(BooksPageActions.deleteBook({ bookId: book.id }));
-
-    this.booksService.delete(book.id).subscribe(() => {
-      this.getBooks();
-      this.removeSelectedBook();
-
-      this.store.dispatch(BooksApiActions.bookDeleted({ bookId: book.id }));
-    });
   }
 }
